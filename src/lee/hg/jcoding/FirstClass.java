1527edd15620fda192679a20fe18861456f5f255
java-dev <java-dev@DESKTOP-7JGGKP2>
2020-10-06 17:52:13 +0900

    num, add

--- src/lee/hg/jcoding/FirstClass.java
+++ src/lee/hg/jcoding/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world~!!");
-
+		int num = 4;
+		int add = num + num;
+		System.out.println("add => " + add);
 	}
 
 }
